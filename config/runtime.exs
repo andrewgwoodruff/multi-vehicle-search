808f13e8484b0785e1b46440dd0546f49c4ae2d9
Andrew Woodruff <agwoodruff93@gmail.com>
2025-05-23 15:26:01 -0600

    update configurations

--- config/runtime.exs
+++ config/runtime.exs
@@ -16,9 +16,6 @@ import Config
 #
 # Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
 # script that automatically sets the env var above.
-if System.get_env("PHX_SERVER") do
-  config :multi_vehicle_search, MultiVehicleSearchWeb.Endpoint, server: true
-end
 
 if config_env() == :prod do
   # The secret key base is used to sign/encrypt cookies and other secrets.
@@ -33,13 +30,13 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
-  port = String.to_integer(System.get_env("PORT") || "4000")
+  host = "multi-vehicle-search.fly.dev"
+  port = String.to_integer(System.get_env("PORT") || "8080")
 
   config :multi_vehicle_search, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :multi_vehicle_search, MultiVehicleSearchWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    url: [host: host, port: 80, scheme: "http"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
@@ -48,7 +45,9 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
-    secret_key_base: secret_key_base
+    secret_key_base: secret_key_base,
+    server: true,
+    force_ssl: false
 
   # ## SSL Support
   #
